d36f01c76a6527a84f191afe8dfb17c10e8f8b4d
Luiz Gustavo Nunes <luiznunes.sm@gmail.com>
2023-01-08 22:32:04 -0300

    added environments config

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,6 +3,27 @@ import paho.mqtt.client as mqtt
 import registers
 from configs import config_broker
 import json
+import os
+
+def set_environments():
+    # Read in the file
+    with open('parameters.ini.sample', 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace('PG_HOST', os.environ['PG_HOST'])
+    filedata = filedata.replace('PG_DATABASE', os.environ['PG_DATABASE'])
+    filedata = filedata.replace('PG_USER', os.environ['PG_USER'])
+    filedata = filedata.replace('PG_PASSWD', os.environ['PG_PASSWD'])
+    filedata = filedata.replace('PG_PORT', os.environ['PG_PORT'])
+
+    filedata = filedata.replace('MQTT_HOST', os.environ['MQTT_HOST'])
+    filedata = filedata.replace('MQTT_PORT', os.environ['MQTT_PORT'])
+
+
+    # Write the file out again
+    with open('parameters.ini', 'w') as file:
+        file.write(filedata)
 
 
 def on_connect(client, userdata, flags, rc):
@@ -27,6 +48,7 @@ def on_message(client, userdata, msg):
         print(f"Unexpected {err=}")
 
 
+set_environments()
 params = config_broker()
 client = mqtt.Client("Backend")
 client.on_connect = on_connect
